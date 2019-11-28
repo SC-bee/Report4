ffd0eaaaa8b7d13254ec4510f4aa24a9499669ff
Shoya Chinen <e195743@ie.u-ryukyu.ac.jp>
2019-11-28 15:14:22 +0900

    step3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
         try {
-            System.out.println(str.length());
-        }catch (NullPointerException n){
+            String str = "百二十三";
+            int val = Integer.parseInt(str);
+            System.out.println(val);
+        } catch (NullPointerException n) {
             System.out.println("NullPointerExceptionエラーが発生しました。");
             System.out.println(n.getMessage());
         }
     }
 }
+
 
